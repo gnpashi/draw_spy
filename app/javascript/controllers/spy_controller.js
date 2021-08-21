001d673b36cf335fba28dc0f756c4c63e4c3c5e2
gnpashi <guynatan@gmail.com>
2021-08-21 03:42:26 +0300

    annoting js

--- app/javascript/controllers/spy_controller.js
+++ app/javascript/controllers/spy_controller.js
@@ -1,18 +1,10 @@
-// Visit The Stimulus Handbook for more details 
-// https://stimulusjs.org/handbook/introduction
-// 
-// This example controller works with specially annotated HTML like:
-//
-// <div data-controller="hello">
-//   <h1 data-target="hello.output"></h1>
-// </div>
-
 import { Controller } from "stimulus"
 
 export default class extends Controller {
     static targets = ["spyOrWord"]
 
     toggle(e) {
+        console.log("togle");
         if (this.spyOrWordTarget.classList.contains('show')) {
             this.spyOrWordTarget.classList.remove('show')
 
